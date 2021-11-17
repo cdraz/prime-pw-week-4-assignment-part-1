8ac4dcc120fda41788de627f47b70cbfd721a6fb
Christopher Razidlo <cd.razidlo@gmail.com>
2021-11-17 12:18:40 -0600

    Completing week 4 part 1 assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,10 +15,11 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
-}
-// Remember to call the function to test
+  return `Hello, ${name}!`;
+} // end helloName
 
+// Remember to call the function to test
+console.log( helloName('Chris') );
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber ) {
