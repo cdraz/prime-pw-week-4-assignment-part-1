f44c3ac3ffec1f4c6a5cd257d0557b48d05c1c81
Christopher Razidlo <cd.razidlo@gmail.com>
2021-11-17 13:58:17 -0600

    completing week 4 part 1 assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -95,23 +95,47 @@ console.log('Finding Necromancer: ', find('Necromancer', dndClasses));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
+  if (letter === string[0]) {
+    return true;
+  } else {
+    return false;
+  }
+} // end isFirstLetter
 
-}
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll(array) {
   let sum = 0
   // TODO: loop to add items
+  for (element of array) {
+    sum += element;
+  }
   return sum;
 }
+// testing sumAll
+console.log('Testing sumAll - 3, 6, 9: ', sumAll([3, 6, 9]));
+console.log('Testing sumAll - 28.2, 11.8, 60: ', sumAll([28.2, 11.8, 60]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+const testInputArray1 = [-10, 4, -6, 0, 1, 2.8, 7];
+const testInputArray2 = [-1, -2, -3, -4, -5, -6, -7.824951]
+const toPositiveArray = (input) => { // trying out function expressions & arrow syntax
+  let positiveArray = [];
+  for (element of input) {
+    if (element > 0) {
+      positiveArray.push(element);
+    }
+  }
+  return positiveArray;
+} // end toPositiveArray
 
-
+//testing toPositiveArray
+console.log(`Testing toPositiveArray - test array ${testInputArray1} :`, toPositiveArray(testInputArray1));
+console.log(`Testing toPositiveArray - test array ${testInputArray2} :`, toPositiveArray(testInputArray2));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
